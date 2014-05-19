3af20881305c54bc18bc24a247aa1610f0dd539e
logofhog <thomasscoggins@gmail.com>
2014-05-19 13:50:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Famtree::Application.config.secret_key_base = 'd29442207eecd14e7c6a712a4300e684c428be808ced99b79133db4de64cfcaeb45c52cb663b291c39bbb168ad6453d4cc854e5f0740066f0609e4f63e9b4920'
