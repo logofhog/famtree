864a05308c73439bcf1a6250508a2f5c39850e83
logofhog <thomasscoggins@gmail.com>
2014-05-22 23:05:56 -0400

    added seed data for family tree

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class WholeFamilyMaker
+  def initialize
+    patriarch = Person.create(:first_name => 'fred', :last_name => 'jones',
+                         :birthday=> '01/01/1920')
+    family = Family.create(:family_name => 'jones', :new_password => 'password')
+    patriarch.update_attribute(:family_id, family)
+    @son_or_daughter = 'Son'
+    @generation = 1
+    add_children(patriarch)
+  end
+
+  def son_or_daughter_toggle
+    if @son_or_daughter == 'Son'
+      @son_or_daughter = 'Daughter'
+    else
+      @son_or_daughter = 'Son'
+    end
+  end
+
+  def add_children(parent)
+    @generation += 1
+    (1..3).each do |u|
+      son_or_daughter_toggle
+      child = Person.create(:first_name => "person#{@son_or_daughter}#{rand(100)}",
+                    :last_name => 'jones')
+      Relationship.create(:person => parent, :relative => child,
+                          :rel_type => @son_or_daughter)
+      if @generation < 4
+        add_children(child)
+      end
+    end
+  end
+end
+
+WholeFamilyMaker.new
