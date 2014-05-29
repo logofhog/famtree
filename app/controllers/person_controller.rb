b08d72a8db3368784896e3a91a0b191a4f5a6e9d
logofhog <thomasscoggins@gmail.com>
2014-05-29 01:07:17 -0400

    added person controller

---
+++ app/controllers/person_controller.rb
@@ -0,0 +1,23 @@
+class PersonController < ApplicationController
+  def index
+    @title = 'Your Family'
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Famtree::Application.routes.draw do
+  resources :person
+
+  root :to => 'person#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root :to => "home#index"
+  # root :to => "home#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
