53cfcda9a79415d21fa948728a4dd6cf00e79ab4
logofhog <thomasscoggins@gmail.com>
2014-07-07 20:59:53 -0400

    changed syntax

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -10,31 +10,32 @@ describe RelationshipsController do
     @user = FactoryGirl.create(:user, :family_id => @family.id)
     @family = FactoryGirl.create :family
     @person.family_id = @family.id
+    @person2 = FactoryGirl.create :person
+    @relationship = Relationship.new(:person => @person,
+                                     :relative => @person2,
+                                     :rel_type => 'Child')
     sign_in @user
   end
 
   describe "GET 'new'" do
+    subject {get 'new', {:id => @user.id } } 
     it "returns http success" do
-      get 'new', {:id => @user.id }
-      response.should be_success
+      expect(subject).to render_template(:new)
     end
   end
 
   describe "POST 'create'" do
-    it "returns http success" do
-      person2 = FactoryGirl.create :person
-      relationship = Relationship.new(:person => @person,
-                                      :relative => person2,
-                                      :rel_type => 'Child')
-      post 'create', {:relationship => relationship.attributes } 
-      response.should redirect_to root_path
+    subject {post 'create', {:relationship => @relationship.attributes } }
+    it "redirects to root" do
+      expect(subject).to redirect_to(root_path)
     end
   end
 
   describe "DELETE 'destroy'" do
-    it "returns http success" do
-      delete 'destroy', {:id => '1'}
-      response.should be_success
+    subject {delete 'destroy', {:id => '1'} }
+    it "redirects to root" do
+      @relationship.save
+      expect(subject).to redirect_to(root_path)
     end
   end
 
